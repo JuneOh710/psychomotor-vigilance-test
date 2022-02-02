fd2eef798104fd57ccf1d05026e7a40385467b0c
June Oh <JuneOh710@gmail.com>
2022-02-02 01:13:04 -0500

    set constants to production

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,10 @@
 // const RESULTS = { totalHit: 0, totalMiss: 0, falseAlarm: 0, reactionTimes: [] }
 const RESULTS = {}
-const IS_PRODUCTION = false;
+const IS_PRODUCTION = true;
 const BODY = document.getElementsByTagName("body")[0];
 let QUESTION_NUMBER = 0;
-const timeout = 1000 * 20;
-const max = 5;
+// const timeout = 1000 * 20;
+// 1000ms * 60 * 5 == 5 min;
+const timeout = 1000 * 60 * 5;
+const max = 10;
 const min = 2;
